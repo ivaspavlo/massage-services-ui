bc5b661a12ab218a5a8a3fa05201c7c39413700e
Pavel Ivashchenko <pivashchenko@greentube.com>
2021-02-11 21:38:27 +0100

    Slider quote: styles partially implemented

---
+++ src/app/modules/ui-partials/slider-quote/container/slider-quote.component.ts
@@ -0,0 +1,35 @@
+
+import { Component, Input, OnInit } from '@angular/core';
+
+interface IQuote {
+  id: string;
+  text: string;
+  author: string;
+  name: string;
+  about: string;
+}
+
+interface IQuoteHexColors {
+  background: string;
+  border: string;
+  text: string;
+  author: string;
+  about: string;
+  controls: string;
+}
+
+@Component({
+  selector: 'app-slider-quote',
+  templateUrl: './slider-quote.component.html',
+  styleUrls: ['./slider-quote.component.scss']
+})
+export class SliderQuoteComponent implements OnInit {
+
+  @Input() quotes: IQuote[];
+  @Input() colours: IQuoteHexColors;
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+}
