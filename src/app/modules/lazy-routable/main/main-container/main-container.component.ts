434dfd29294f334d231e60f1c7937847f9809d4f
Pavel Ivashchenko <pivashchenko@greentube.com>
2021-02-13 15:40:52 +0100

    Slider quote: complete

--- src/app/modules/lazy-routable/main/main-container/main-container.component.ts
+++ src/app/modules/lazy-routable/main/main-container/main-container.component.ts
@@ -27,6 +27,45 @@ const PRODUCTS = [
   { background: '/assets/img/png/service-bg.png', title: 'CARD1', info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.', button: 'TEST' }
 ];
 
+const QUOTES = [
+  {
+    id: 'quote_1',
+    text: 'Spread love everywhere you go. Let no one ever come to you without leaving happier',
+    name: 'Mother Teresa',
+    about: 'Nobel Peace Prize winner',
+    img: '/assets/img/png/profile.png',
+    socialNetwork: 'facebook',
+    socialLink: '#',
+    socialImg: '/assets/img/png/profile.png'
+  }, {
+    id: 'quote_2',
+    text: 'Spread love everywhere you go. Let no one ever come to you without leaving happier',
+    name: 'Mother Teresa',
+    about: 'Nobel Peace Prize winner',
+    socialNetwork: 'instagram',
+    socialLink: '#',
+    socialImg: '/assets/img/png/profile.png'
+  }, {
+    id: 'quote_3',
+    text: 'Spread love everywhere you go. Let no one ever come to you without leaving happier',
+    name: 'Mother Teresa',
+    about: 'Nobel Peace Prize winner',
+    img: '/assets/img/png/profile.png',
+    socialNetwork: 'linkedin',
+    socialLink: '#',
+    socialImg: '/assets/img/png/profile.png'
+  }, {
+    id: 'quote_4',
+    text: 'Spread love everywhere you go. Let no one ever come to you without leaving happier',
+    name: 'Mother Teresa',
+    about: 'Nobel Peace Prize winner',
+    img: '/assets/img/png/profile.png',
+    socialNetwork: 'facebook',
+    socialLink: '#',
+    socialImg: '/assets/img/png/profile.png'
+  }
+];
+
 @Component({
   selector: 'app-main-container',
   templateUrl: './main-container.component.html',
@@ -38,6 +77,7 @@ export class MainContainerComponent implements OnInit {
   public socialButtons = SOCIAL_BUTTONS;
   public footerItems = FOOTER_MENU_ITEMS;
   public productCards = PRODUCTS;
+  public quotes = QUOTES;
 
   public isScrollBtnVisible = true;
 
