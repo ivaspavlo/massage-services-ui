782fd823511954f1556ce822d4c868cddbe16e8d
Pavel Ivashchenko <pivashchenko@greentube.com>
2021-02-21 00:34:43 +0100

    Contact us: input turn to controlValueAccessor

--- src/app/modules/ui-partials/input/container/input.component.ts
+++ src/app/modules/ui-partials/input/container/input.component.ts
@@ -1,17 +1,57 @@
 
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
+
+interface IInputConfig {
+  label?: string;
+  placeholder?: string;
+  inputType?: string;
+  id?: string;
+}
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: InputComponent,
+    multi: true
+  }],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, ControlValueAccessor {
+
+  @Input() set config(value: IInputConfig) {
+    this.label = value?.label || '';
+    this.placeholder = value?.placeholder || '';
+    this.inputType = value?.inputType || 'text';
+    this.inputId = value?.id || this.getRandomId();
+  };
+  public label: string;
+  public placeholder: string;
+  public inputType: string;
+  public inputId: string;
+  public value: unknown;
+
+  private onChange;
+  private onTouched;
 
   constructor() { }
 
   ngOnInit(): void { }
+
+  public writeValue(value: unknown): void { this.value = value; }
+  public registerOnChange(fn): void { this.onChange = fn; }
+  public registerOnTouched(fn): void { this.onChange = fn; }
+
+  /**
+   * Return string with randomly generated number
+   * @returns {string}
+   */
+  private getRandomId(): string {
+    return `input_id_${Math.floor(Math.random() * 6) + 1}`;
+  }
 
 }
