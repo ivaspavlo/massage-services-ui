6a1ef13efb940232b3bdbca39994eda1d347efa0
Pavel Ivashchenko <pivashchenko@greentube.com>
2021-03-25 17:54:51 +0100

    Tooltip: complete

--- src/app/modules/ui-partials/tooltip/container/tooltip.component.ts
+++ src/app/modules/ui-partials/tooltip/container/tooltip.component.ts
@@ -1,5 +1,9 @@
 
-import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, HostBinding, Inject, OnInit, ViewChild } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+import { ContentTypes, Positions } from '../constants';
+import { ITooltipConfig } from '../interfaces';
 
 
 @Component({
@@ -10,26 +14,63 @@ import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, E
 })
 export class TooltipComponent implements OnInit, AfterViewInit {
   
-  @ViewChild('container', {read: ElementRef}) private container;
+  @ViewChild('container', {read: ElementRef}) private container: ElementRef;
   
   public top: string;
   public left: string;
+  public hasTemplate = false;
+  private offsetPx = 10;
 
   constructor(
-    @Inject('tooltipConfig') private config,
+    @Inject('tooltipConfig') private config: ITooltipConfig,
+    @Inject(DOCUMENT) private document: Document,
     private cdr: ChangeDetectorRef
   ) { }
-
-  ngOnInit(): void { debugger; }
+  
+  ngOnInit() {
+    this.hasTemplate = this.config.contentType !== ContentTypes.string;
+  }
   
   ngAfterViewInit(): void {
-    debugger;
-    const { offsetTop: hostTop, offsetLeft: hostLeft } = this.config.host;
-    const { width: hostWidth } = this.config.host.getBoundingClientRect();
-    const { height } = this.container.nativeElement.getBoundingClientRect();
-    this.top = `${hostTop - height}px`;
-    this.left = `${hostLeft + hostWidth}px`;
+    this.setPosition();
     this.cdr.detectChanges();
+  }
+  
+  private setPosition(): void {
+    const { offsetTop: hostTop, offsetLeft: hostLeft } = this.config.host;
+    const { width: hostWidth, height: hostHeight } = this.config.host.getBoundingClientRect();
+    const { height: tooltipHeight, width: tooltipWidth } = this.container.nativeElement.getBoundingClientRect();
+
+    this.top = this.getVerticalPosition(this.config.position === Positions.below, hostTop, hostHeight, tooltipHeight);
+    this.left = this.getHorisontalPosition(this.config.position === Positions.right, hostLeft, hostWidth, tooltipWidth);
+  }
+  
+  private getVerticalPosition(isAbove: boolean, hostTop: number, hostHeight: number, tooltipHeight: number): string {
+    const positionAbove = hostTop - tooltipHeight - this.offsetPx;
+    const positionBelow = hostTop + hostHeight + this.offsetPx;
+    const fitsAbove = this.fitsVertical(positionAbove, tooltipHeight);
+    const fitsBelow = this.fitsVertical(positionBelow, tooltipHeight);
+    return isAbove ?
+      `${fitsAbove ? positionAbove : positionBelow}px` :
+      `${fitsBelow ? positionBelow : positionAbove}px`;
+  }
+  
+  private getHorisontalPosition(isLeft: boolean, hostLeft: number, hostWidth: number, tooltipWidth: number): string {
+    const positionToLeft = hostLeft - tooltipWidth - this.offsetPx;
+    const positionToRight = hostLeft + hostWidth + this.offsetPx;
+    const fitsLeft = this.fitsHorisontal(positionToLeft, tooltipWidth);
+    const fitsRight = this.fitsHorisontal(positionToRight, tooltipWidth);
+    return isLeft ?
+      `${fitsLeft ? positionToLeft : positionToRight}px` :
+      `${fitsRight ? positionToRight : positionToLeft}px`;
+  }
+  
+  private fitsVertical(tooltipTop: number, tooltipHeight: number): boolean {
+    return tooltipTop >= 0 && tooltipTop + tooltipHeight <= this.document.documentElement.clientHeight;
+  }
+  
+  private fitsHorisontal(tooltipLeft: number, tooltipWidth: number): boolean {
+    return tooltipLeft >= 0 && tooltipLeft + tooltipWidth <= this.document.documentElement.clientWidth;
   }
 
 }
--- src/app/modules/ui-partials/tooltip/directives/tooltip.directive.ts
+++ src/app/modules/ui-partials/tooltip/directives/tooltip.directive.ts
@@ -1,6 +1,7 @@
 
 import { ComponentFactoryResolver, ComponentRef, Directive, ElementRef, HostListener, Injector, Input, Renderer2, TemplateRef, Type, ViewContainerRef } from '@angular/core';
 import { TooltipComponent } from '../container/tooltip.component';
+import { ContentTypes, Positions } from '../constants';
 
 
 @Directive({
@@ -9,18 +10,25 @@ import { TooltipComponent } from '../container/tooltip.component';
 export class TooltipDirective {
 
   @Input('appTooltip') content: string | TemplateRef<any> | Type<any>;
+  @Input('tooltipPosition') position: keyof Positions;
   
   @HostListener('mouseenter')
   mouseenter() {
-    debugger;
     if (this.componentRef) {
       return;
     };
     const factory = this.resolver.resolveComponentFactory(TooltipComponent);
+    const contentType = this.getContentType();
+    const content = this.generateNgContent(this.content, contentType);
     const injector = Injector.create({
-      providers: [{provide: 'tooltipConfig', useValue: { host: this.element.nativeElement }}]
+      providers: [
+        {
+          provide: 'tooltipConfig',
+          useValue: { contentType, host: this.element.nativeElement, position: this.position }
+        }
+      ]
     });
-    this.componentRef = this.vcr.createComponent(factory, 0, injector, this.generateNgContent());
+    this.componentRef = this.vcr.createComponent(factory, 0, injector, content);
   }
   @HostListener('mouseout')
   mouseout() {
@@ -35,24 +43,46 @@ export class TooltipDirective {
     private injector: Injector,
     private resolver: ComponentFactoryResolver,
     private vcr: ViewContainerRef
-  ) { debugger; }
+  ) { }
 
-  generateNgContent() {
-    if (typeof this.content === 'string') {
-      const element = this.renderer.createText(this.content);
-      return [ [ element ] ];
-    } else if (this.content instanceof TemplateRef) {
-      const context = {};
-      const viewRef = this.content.createEmbeddedView(context);
-      return [ viewRef.rootNodes ];
-    } else {
-      const factory = this.resolver.resolveComponentFactory(this.content);
-      const componentRef = factory.create(this.injector);
-      return [ [ componentRef.location.nativeElement ] ];
+  private generateNgContent(content, contentType: string): any[][] | undefined {
+    switch (contentType) {
+      case ContentTypes.string: {
+        const element = this.renderer.createText(content);
+        return [ [element] ];
+      };
+      case ContentTypes.template: {
+        const context = {};
+        const viewRef = content.createEmbeddedView(context);
+        return [ viewRef.rootNodes ];
+      };
+      case ContentTypes.component: {
+        const factory = this.resolver.resolveComponentFactory(content);
+        const componentRef = factory.create(this.injector);
+        return [ [ componentRef.location.nativeElement ] ];
+      };
+      case ContentTypes.other: {
+        return undefined;
+      }
     }
   }
   
-  destroy() {
+  private getContentType(): string {
+    if (typeof this.content === 'string') {
+      return ContentTypes.string;
+    } else if (this.content instanceof TemplateRef) {
+      return ContentTypes.template;
+    } else {
+      try {
+        this.resolver.resolveComponentFactory(this.content);
+        return ContentTypes.component;
+      } catch(error) {
+        return ContentTypes.other;
+      }
+    }
+  }
+  
+  private destroy(): void {
     this.componentRef && this.componentRef.destroy();
     this.componentRef = null;
   }
