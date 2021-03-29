07dea10bf125c1af25d1e80c73ac7b7959d2548b
Pavel Ivashchenko <pivashchenko@greentube.com>
2021-03-29 19:08:28 +0200

    Dialog: started implementation

---
+++ src/app/modules/ui-partials/dialog/services/dialog.service.ts
@@ -0,0 +1,12 @@
+
+import { Injectable } from '@angular/core';
+import { DialogModule } from '../dialog.module';
+
+
+@Injectable({
+  providedIn: DialogModule
+})
+export class DialogService {
+
+  constructor() { }
+}
