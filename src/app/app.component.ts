233c9e725abcc2ca46909f2a97f3f04923c8c4b9
Pavel Ivashchenko <pivashchenko@greentube.com>
2021-04-10 18:53:10 +0200

    Core: experimenting with locomotive scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+
+import { DOCUMENT } from '@angular/common';
+import { Component, ElementRef, Inject, ViewChild } from '@angular/core';
+import { ResizeObserver } from '@juggle/resize-observer';
+import LocomotiveScroll from 'locomotive-scroll';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'm1';
+  
+  @ViewChild('scrollContent') scrollContent: ElementRef;
+  
+  public title = 'm1';
+  public scroll: LocomotiveScroll;
+  
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+  
+  ngOnInit() {
+    this.scroll = new LocomotiveScroll({
+      el: this.document.querySelector('[data-scroll-container]'),
+      smooth: true,
+      getDirection: true
+    });
+    const test = this.document.querySelector('[data-scroll-container]');
+    debugger;
+  }
+  
+  ngAfterViewInit() {
+    const ro = new ResizeObserver((entries, observer) => {
+      console.log('resize')
+      entries.forEach((entry, index) => {
+        const { inlineSize: width, blockSize: height } = entry.contentBoxSize[0];
+        if(this.scroll) {
+          debugger;
+          this.scroll.update();
+        }
+      });
+    });
+
+    ro.observe(this.scrollContent.nativeElement);
+  }
+  
 }
