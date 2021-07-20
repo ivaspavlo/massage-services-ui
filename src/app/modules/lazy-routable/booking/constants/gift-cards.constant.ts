307a61b9760277edbcb98b5051cb89aed8e7b9eb
Pavel Ivashchenko <pivashchenko@greentube.com>
2021-07-20 20:26:07 +0200

    Booking: gift cards translation partially implemented

--- src/app/modules/lazy-routable/booking/constants/gift-cards.constant.ts
+++ src/app/modules/lazy-routable/booking/constants/gift-cards.constant.ts
@@ -7,25 +7,25 @@ export const GiftCards: IProductCard[] = [
     id: '1',
     background: '/assets/img/png/service-bg.png',
     title: 'booking.gift.one.title',
-    info: 'booking.gift.one.info',
+    info: 'booking.gift.info',
     button: 'booking.gift.buy-now'
   }, {
     id: '2',
     background: '/assets/img/png/service-bg.png',
     title: 'booking.gift.two.title',
-    info: 'booking.gift.two.info',
+    info: 'booking.gift.info',
     button: 'booking.gift.buy-now'
   }, {
     id: '3',
     background: '/assets/img/png/service-bg.png',
     title: 'booking.gift.three.title',
-    info: 'booking.gift.three.info',
+    info: 'booking.gift.t.info',
     button: 'booking.gift.buy-now'
   }, {
     id: '4',
     background: '/assets/img/png/service-bg.png',
     title: 'booking.gift.four.title',
-    info: 'booking.gift.four.info',
+    info: 'booking.gift.info',
     button: 'booking.gift.buy-now'
   }
 ];
