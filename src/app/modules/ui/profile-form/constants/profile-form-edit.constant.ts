8f007e4ea69ca0d885a141a210289e9a0ecd144d
Pavel Ivashchenko <pivashchenko@greentube.com>
2021-08-16 19:19:29 +0200

    Profile: fixed bug with undefined input values

--- src/app/modules/ui/profile-form/constants/profile-form-edit.constant.ts
+++ src/app/modules/ui/profile-form/constants/profile-form-edit.constant.ts
@@ -5,13 +5,11 @@ import { ProfileFormSettings } from './profile-form-settings.constant';
 
 export class ProfileFormEdit {
   constructor(initValue: IProfileFormValue | null) {
-    debugger;
     ProfileFormSettings
       .filter(ctrl => ctrl.name !== 'password' && ctrl.name !== 'confirmPassword')
       .forEach(ctrl => {
-        debugger
         this[ctrl.name] = new FormControl(
-          { value: initValue ? initValue[ctrl.name] : '', disabled: true },
+          { value: initValue && initValue[ctrl.name] ? initValue[ctrl.name] : '', disabled: true },
           ctrl.validators
         );
       });
