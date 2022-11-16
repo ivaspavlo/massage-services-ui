379e98c18067595f9afe047471de6f77186290a8
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2022-11-16 16:56:49 +0100

    ST-17: Partially implement qty picker

---
+++ src/app/modules/ui/qty-selector/components/index.ts
@@ -0,0 +1,6 @@
+import { QtySelectorComponent } from "./qty-selector/qty-selector.component";
+
+
+export const COMPONENTS = [
+  QtySelectorComponent
+];
---
+++ src/app/modules/ui/qty-selector/qty-selector.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { COMPONENTS } from './components';
+
+
+@NgModule({
+  declarations: [
+    ...COMPONENTS
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ...COMPONENTS
+  ]
+})
+export class QtySelectorModule { }
