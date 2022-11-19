7a62034c28c65216f3f54dd2bd00b586df84d994
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2022-11-19 15:36:48 +0100

    ST-21: Remove unused code

--- src/app/shared/pipes/index.ts
+++ src/app/shared/pipes/index.ts
@@ -1,10 +1,8 @@
 import { FillPipe } from './fill.pipe';
 import { TranslateArrPipe } from './translate-arr.pipe';
-import { TranslateObjPipe } from './translate-obj.pipe';
 
 
 export const APP_PIPES = [
   FillPipe,
-  TranslateObjPipe,
   TranslateArrPipe
 ];
