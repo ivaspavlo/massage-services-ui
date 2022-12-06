288e46d8a9e5eaf6e3602da1c26ccfe65f028897
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2022-12-06 22:07:43 +0100

    ST-23.15: Add styles to upload image modal

--- src/app/modules/lazy-routable/profile/modals/index.ts
+++ src/app/modules/lazy-routable/profile/modals/index.ts
@@ -1,6 +1,8 @@
 import { EditProfileModalComponent } from "./edit-profile-modal/edit-profile-modal.component";
+import { UploadAvatarModalComponent } from "./upload-avatar-modal/upload-avatar-modal.component";
 
 
 export const MODALS = [
-  EditProfileModalComponent
+  EditProfileModalComponent,
+  UploadAvatarModalComponent
 ];
